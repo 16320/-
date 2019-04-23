746e9e6faada45e9c8a37e9e40b06e8d904062b0
16320 <44873664+16320@users.noreply.github.com>
2019-04-23 08:48:10 +0300

    Add files via upload

---
+++ SELECTION SORT.java
@@ -0,0 +1,36 @@
+
+public class main {
+	void sort(int arr[]) 
+    { 
+        int n = arr.length; 
+        for (int i = 0; i < n-1; i++) 
+        { 
+            int min_idx = i; 
+            for (int j = i+1; j < n; j++) 
+                if (arr[j] < arr[min_idx]) 
+                    min_idx = j; 
+            int temp = arr[min_idx]; 
+            arr[min_idx] = arr[i]; 
+            arr[i] = temp; 
+        } 
+    } 
+    void printArray(int arr[]) 
+    { 
+        int n = arr.length; 
+        for (int i=0; i<n; ++i) 
+            System.out.print(arr[i]+" "); 
+        System.out.println(); 
+    } 
+    public static void main(String args[]) 
+    { 
+        main ob = new main(); 
+        int arr[] = {9,45,3,86,1}; 
+        ob.sort(arr);  
+        ob.printArray(arr); 
+    } 
+} 
+
+
+	
+
+
